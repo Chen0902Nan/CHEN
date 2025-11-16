f2c351e752df73290846c7f81b1a14abeffe39d5
Chen0902 <1094536408@qq.com>
2025-11-16 14:17:04 +0800

    leetCode

--- leetcode/Karl/59.js
+++ leetcode/Karl/59.js
@@ -4,32 +4,35 @@
  */
 var generateMatrix = function (n) {
   let res = new Array(n).fill(0).map(() => new Array(n).fill(0))
-  let startX = 0,
-    startY = 0
   let loop = Math.floor(n / 2)
-  let mid = Math.floor(n / 2)
   let count = 1
   let offset = 1
+  let mid = Math.floor(n / 2)
+  let startX = 0,
+    startY = 0
+
   while (loop--) {
-    let x = startX
-    let y = startY
-    for (; y < startY + n - offset; y++) {
-      res[x][y] = count++
+    for (let val = 1; val < n * n; val++) {
+      let x = startX,
+        y = startY
+      for (; y < startY + n - offset; y++) {
+        res[x][y] = count++
+      }
+      for (; x < startX + n - offset; x++) {
+        res[x][y] = count++
+      }
+      for (; y > startY; y--) {
+        res[x][y] = count++
+      }
+      for (; x > startX; x--) {
+        res[x][y] = count++
+      }
+      startX++
+      startY++
+      offset = offset + 2
     }
-    for (; x < startX + n - offset; x++) {
-      res[x][y] = count++
-    }
-    for (; y > startY; y--) {
-      res[x][y] = count++
-    }
-    for (; x > startX; x--) {
-      res[x][y] = count++
-    }
-    offset = offset + 2
-    startX++
-    startY++
   }
-  if (n % 2 === 1) {
+  if (n % 2 == 1) {
     res[mid][mid] = count
   }
   return res
