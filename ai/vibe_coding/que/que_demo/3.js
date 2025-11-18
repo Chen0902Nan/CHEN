965695ddf396e8a33f71dc31b0aa5a79f8ff37ef
Chen0902 <1094536408@qq.com>
2025-11-18 23:19:52 +0800

    modelscope

---
+++ ai/vibe_coding/que/que_demo/3.js
@@ -0,0 +1,11 @@
+/**
+ * @func 查找所有非活跃用户
+ * @param users 用户列表
+ * @return 非活跃用户列表
+ *
+ */
+
+async function getInactiveUsers (users) {
+  const inactiveUsers = await users.filter(user => user.status === 'INACTIVE')
+  return inactiveUsers
+}
