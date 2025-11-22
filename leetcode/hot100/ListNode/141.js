dcfd969d1ef4415413c873d35dbd830ba59210d7
Chen0902 <1094536408@qq.com>
2025-11-23 00:36:21 +0800

    归并

---
+++ leetcode/hot100/ListNode/141.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function (head) {
+  if (!head || !head.next) {
+    return false
+  }
+  let fast = head
+  let slow = head
+  while (fast && fast.next) {
+    fast = fast.next.next
+    slow = slow.next
+    if (slow == fast) {
+      return true
+    }
+  }
+  return false
+}
