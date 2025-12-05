642432e8348a2325f42db2a8bcfc1bff4e176e55
Chen0902 <1094536408@qq.com>
2025-12-05 23:55:16 +0800

    KMP

--- leetcode/Karl/str/28.js
+++ leetcode/Karl/str/28.js
@@ -25,21 +25,54 @@
 // }
 
 // console.log(strStr('sadbutsad', 'sad'))
+// /**
+//  * @param {string} haystack
+//  * @param {string} needle
+//  * @return {number}
+//  */
+// var strStr = function (haystack, needle) {
+//   const hLen = haystack.length - 1
+//   const nLen = needle.length - 1
+//   if (needle == '' || hLen < nLen) return -1
+//   for (let i = 0; i <= hLen - nLen; i++) {
+//     let str = haystack.slice(i, i + nLen + 1)
+//     if (str === needle) {
+//       return i
+//     } else {
+//       continue
+//     }
+//   }
+//   return -1
+// }
+
 /**
  * @param {string} haystack
  * @param {string} needle
  * @return {number}
  */
 var strStr = function (haystack, needle) {
-  const hLen = haystack.length - 1
-  const nLen = needle.length - 1
-  if (needle == '' || hLen < nLen) return -1
-  for (let i = 0; i <= hLen - nLen; i++) {
-    let str = haystack.slice(i, i + nLen + 1)
-    if (str === needle) {
-      return i
-    } else {
-      continue
+  let hLen = haystack.length
+  let nLen = needle.length
+  if (needle == '' || hLen - nLen < 0) return -1
+  const pi = new Array(nLen).fill(0)
+  for (let i = 1, j = 0; i < nLen; i++) {
+    while (j > 0 && needle[i] !== needle[j]) {
+      j = pi[j - 1]
+    }
+    if (needle[i] == needle[j]) {
+      j++
+    }
+    pi[i] = j
+  }
+  for (let i = 0, j = 0; i < hLen; i++) {
+    while (j > 0 && haystack[i] !== needle[j]) {
+      j = pi[j - 1]
+    }
+    if (haystack[i] == needle[j]) {
+      j++
+    }
+    if (j == nLen) {
+      return i - nLen + 1
     }
   }
   return -1
