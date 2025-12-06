7274c42dcb3b97a4b408e73d30e2c84d13278f5e
Chen0902 <1094536408@qq.com>
2025-12-06 16:51:13 +0800

    stream

---
+++ ai/streaming/demo/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="wrapper">
+    <img alt="Vue logo" class="logo" src="https://vitejs.dev/logo.svg" />
+    <h1>Vue + Vite (JavaScript)</h1>
+    <p>项目已初始化，开始愉快地编码吧！</p>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
+
+<style scoped>
+.wrapper {
+  font-family: system-ui, -apple-system, Segoe UI, Roboto, Ubuntu,
+    Cantarell, Noto Sans, "Helvetica Neue", Arial, "Apple Color Emoji",
+    "Segoe UI Emoji";
+  padding: 2rem;
+  text-align: center;
+}
+.logo {
+  height: 64px;
+}
+</style>
