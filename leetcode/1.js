8afbffde6ff4bc43c5c9931ebddff2209e96b5ea
Chen0902 <1094536408@qq.com>
2025-12-14 22:25:12 +0800

    111

--- leetcode/1.js
+++ leetcode/1.js
@@ -38,7 +38,3 @@ const check = (code, isActive) => {
     return isActive;
 }
 
-作者：力扣官方题解
-链接：https://leetcode.cn/problems/coupon-code-validator/solutions/3853498/you-hui-quan-xiao-yan-qi-by-leetcode-sol-6noh/
-来源：力扣（LeetCode）
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
