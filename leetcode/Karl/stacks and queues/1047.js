16fe5c92d72dd1078c9b990da28f3ab7fdc47c46
Chen0902 <1094536408@qq.com>
2025-12-15 23:40:45 +0800

    stack queues

---
+++ leetcode/Karl/stacks and queues/1047.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var removeDuplicates = function(s) {
+    const stack=[]
+    for(const x of s){
+        if(stack[stack.length-1]==x){
+          stack.pop()
+        }else{
+          stack.push(x)
+        }
+    }
+    return stack.join('')
+};
+removeDuplicates('abbaca')
