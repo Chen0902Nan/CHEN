19c56ef83a1adc2aa9773bab2ce20686dfddaf86
Chen0902 <1094536408@qq.com>
2025-12-18 01:51:50 +0800

    leetcode

---
+++ algorithm/arr_sort_bind/1.js
@@ -0,0 +1,15 @@
+var merge = function (nums1, m, nums2, n) {
+  // 正向填入 可能会出现覆盖的情况
+  let p1 = m - 1,
+    p2 = n - 1,
+    p = m + n - 1;
+  while (p2 >= 0) {
+    // nums2 还有要合并的元素
+    // 如果 p1 < 0，那么走 else 分支，把 nums2 合并到 nums1 中
+    if (p1 >= 0 && nums1[p1] > nums2[p2]) {
+      nums1[p--] = nums1[p1--]; // 填入 nums1[p1]
+    } else {
+      nums1[p--] = nums2[p2--]; // 填入 nums2[p1]
+    }
+  }
+};
