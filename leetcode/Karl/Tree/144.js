74b96d35647ca1563e91642fa304d81fc34e2f37
Chen0902 <1094536408@qq.com>
2025-12-25 22:35:53 +0800

    tree

--- leetcode/Karl/Tree/144.js
+++ leetcode/Karl/Tree/144.js
@@ -10,7 +10,7 @@
  * @param {TreeNode} root
  * @return {number[]}
  */
-// 前序遍历:
+// 递归前序遍历:
 
 var preorderTraversal = function (root, res = []) {
   if (!root) return res;
@@ -20,7 +20,7 @@ var preorderTraversal = function (root, res = []) {
   return res;
 };
 
-// 中序遍历:
+// 递归中序遍历:
 
 var inorderTraversal = function (root, res = []) {
   if (!root) return res;
@@ -30,12 +30,59 @@ var inorderTraversal = function (root, res = []) {
   return res;
 };
 
-// 后序遍历:
+// 递归后序遍历:
 
 var postorderTraversal = function (root, res = []) {
   if (!root) return res;
   postorderTraversal(root.left, res);
   postorderTraversal(root.right, res);
   res.push(root.val);
+  return res;
+};
+
+// 迭代前序遍历：
+var preorderTraversal = function (root) {
+  const res = [];
+  const stack = [];
+  while (stack.length || root) {
+    while (root) {
+      res.push(root.val);
+      stack.push(root);
+      root = root.left;
+    }
+    root = stack.pop();
+    root = root.right;
+  }
+  return res;
+};
+// 迭代中序遍历：
+var inorderTraversal = function (root) {
+  const stack = [];
+  const res = [];
+  while (root || stack.length) {
+    while (root) {
+      stack.push(root);
+      root = root.left;
+    }
+    root = stack.pop();
+    res.push(root.val);
+    root = root.right;
+  }
+  return res;
+};
+// 迭代后序遍历：
+var postorderTraversal = function (root) {
+  // 初始化数据
+  const res = [];
+  const stack = [];
+  while (root || stack.length) {
+    while (root) {
+      stack.push(root);
+      res.unshift(root.val);
+      root = root.right;
+    }
+    root = stack.pop();
+    root = root.left;
+  }
   return res;
 };
