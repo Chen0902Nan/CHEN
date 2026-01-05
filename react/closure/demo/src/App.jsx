64bfebfe9a6cd98d4b41b236bd23bb20fe9f756c
Chen0902 <1094536408@qq.com>
2026-01-05 20:50:28 +0800

    useMemo

---
+++ react/closure/demo/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+export default function App() {
+  const [count, setCount] = useState(0);
+  // useEffect(() => {
+  //   const timer = setInterval(() => {
+  //     console.log("Current count:", count);
+  //   }, 1000);
+  //   return () => clearInterval(timer);
+  // }, []);
+  useEffect(() => {
+    const timer = setInterval(() => {
+      console.log("Current count:", count);
+    }, 1000);
+    // 不只是组件卸载执行
+    // 每次effect 重新执行之前 都会执行上一次的清理函数
+    return () => clearInterval(timer);
+  }, [count]);
+  console.log("current Count:-----", count);
+
+  return (
+    <>
+      <p>{count}</p>
+      <button onClick={() => setCount(count + 1)}>count++</button>
+    </>
+  );
+}
