053b675c62164b8a4743f2f7e8231056e5f5f9d8
Chen0902 <1094536408@qq.com>
2026-01-14 21:20:44 +0800

    TypeScript

---
+++ project/ai_fullstack/ts-demo/2.js
@@ -0,0 +1,6 @@
+// 强类型可以杜绝90%的问题
+function addTs(a, b) {
+    return a + b;
+}
+var result = addTs(1, 2);
+console.log(result);
---
+++ project/ai_fullstack/ts-demo/3.ts
@@ -0,0 +1,6 @@
+// ts 是js的超集，把ts当作js来写
+function getArea(width: number, height: number): number {
+  return width * height;
+}
+const area: number = getArea(20, 5);
+console.log(area);
