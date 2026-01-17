a6b13b49aa76beb5be7c9a5a82c5da77ee9b7549
Chen0902 <1094536408@qq.com>
2026-01-17 13:37:23 +0800

    css在项目中如何解决父子冲突

---
+++ react/module_css/vue-css-demo/src/components/HelloWorld.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <h1 class="txt">你好 世界！</h1>
+    <h1 class="txt2">一点点</h1>
+  </div>
+
+</template>
+
+<style scoped>
+.txt {
+  color: blue;
+}
+
+.txt2 {
+  color: orange;
+}
+</style>
