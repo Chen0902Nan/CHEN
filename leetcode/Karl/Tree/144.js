2b900aa658afae1686e979feec7e59416304ae7c
Chen0902 <1094536408@qq.com>
2026-03-07 00:57:37 +0800

    tree

--- leetcode/Karl/Tree/144.js
+++ leetcode/Karl/Tree/144.js
@@ -10,79 +10,69 @@
  * @param {TreeNode} root
  * @return {number[]}
  */
-// 递归前序遍历:
-
-var preorderTraversal = function (root, res = []) {
-  if (!root) return res;
-  res.push(root.val);
-  preorderTraversal(root.left, res);
-  preorderTraversal(root.right, res);
-  return res;
-};
-
-// 递归中序遍历:
-
-var inorderTraversal = function (root, res = []) {
-  if (!root) return res;
-  inorderTraversal(root.left, res);
-  res.push(root.val);
-  inorderTraversal(root.right, res);
-  return res;
-};
-
-// 递归后序遍历:
-
-var postorderTraversal = function (root, res = []) {
-  if (!root) return res;
-  postorderTraversal(root.left, res);
-  postorderTraversal(root.right, res);
-  res.push(root.val);
-  return res;
-};
-
-// 迭代前序遍历：
 var preorderTraversal = function (root) {
+  const stack = [root];
   const res = [];
-  const stack = [];
-  while (stack.length || root) {
-    while (root) {
-      res.push(root.val);
-      stack.push(root);
-      root = root.left;
-    }
-    root = stack.pop();
-    root = root.right;
+  if (!root) return res;
+  let cur = null;
+  while (stack.length) {
+    cur = stack.pop();
+    res.push(cur.val);
   }
   return res;
 };
-// 迭代中序遍历：
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var postorderTraversal = function (root) {
+  const stack = [root];
+  const res = [];
+  if (!root) return res;
+  let cur = null;
+  while (stack.length) {
+    cur = stack.pop();
+    res.push(cur.val);
+    cur.left && stack.push(cur.left);
+    cur.right && stack.push(cur.right);
+  }
+  return res.reverse();
+};
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
 var inorderTraversal = function (root) {
   const stack = [];
   const res = [];
-  while (root || stack.length) {
-    while (root) {
-      stack.push(root);
-      root = root.left;
+  let cur = root;
+  while (stack.length || cur) {
+    if (cur) {
+      stack.push(cur);
+      cur = cur.left;
+    } else {
+      cur = stack.pop();
+      res.push(cur.val);
+      cur = cur.right;
     }
-    root = stack.pop();
-    res.push(root.val);
-    root = root.right;
-  }
-  return res;
-};
-// 迭代后序遍历：
-var postorderTraversal = function (root) {
-  // 初始化数据
-  const res = [];
-  const stack = [];
-  while (root || stack.length) {
-    while (root) {
-      stack.push(root);
-      res.unshift(root.val);
-      root = root.right;
-    }
-    root = stack.pop();
-    root = root.left;
   }
   return res;
 };
---
+++ leetcode/Karl/twoPointer/11.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function (height) {
+  let res = 0;
+  let left = 0,
+    right = height.length - 1;
+  while (left < right) {
+    res = Math.max(Math.min(height[left], height[right]) * (right - left), res);
+    if (height[left] <= height[right]) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+  return res;
+};
+
+
