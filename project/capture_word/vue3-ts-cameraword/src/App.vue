362836613f10bf306a50134dd2730d0f4ce07d86
Chen0902 <1094536408@qq.com>
2026-03-07 16:38:09 +0800

    feat: 初始化 Vue3 + TypeScript + Vite 项目骨架及核心组件
    - 新增项目基础配置，包括 Vite、TypeScript、Vue 插件及构建脚本
    - 添加核心组件 PictureCard，支持图片上传和预览功能
    - 引入样式重置和基础布局，为拍照记单词应用搭建前端框架

---
+++ project/capture_word/vue3-ts-cameraword/src/App.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import PictureCard from './components/PictureCard.vue'
+// composition api 组合式API
+import { ref } from 'vue'
+
+const props = defineProps({
+  word: {
+    type: String,
+    default: ''
+  },
+  audio: {
+    type: String,
+    default: ''
+  }
+})
+
+const word = ref('请上传图片')
+const audio = ref('')
+const update = async (imageDate: string) => {
+  console.log(imageDate);
+
+}
+const submit = (imageData: string) => {
+  console.log(imageData);
+
+}
+
+
+
+
+</script>
+
+<template>
+  <div class="container">
+    <PictureCard :word="word" :audio="audio" @update-image="submit" />
+  </div>
+</template>
+
+
+<style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0;
+  padding: 0;
+  width: 100vw;
+  height: 100vh;
+  font-size: .85rem;
+  background: linear-gradient(180deg, rgb(235, 189, 161) 0%, rgb(71, 49, 32) 100%);
+}
+</style>
